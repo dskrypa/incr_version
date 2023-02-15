d787bdfe10be33d9219402057571f670a1fae7a7
dskrypa <dskrypa@gmail.com>
2023-02-15 06:46:57 -0500

    converted from setup.py to setup.cfg

--- src/incr_version/__version__.py
+++ src/incr_version/__version__.py
@@ -1,8 +1,7 @@
-
 __title__ = 'incr_version'
 __description__ = 'Python project version auto-incrementer'
 __url__ = 'https://github.com/dskrypa/incr_version'
-__version__ = '2022.02.26'
+__version__ = '2023.02.15'
 __author__ = 'Doug Skrypa'
 __author_email__ = 'dskrypa@gmail.com'
-__copyright__ = 'Copyright 2020 Doug Skrypa'
+__copyright__ = 'Copyright 2023 Doug Skrypa'
